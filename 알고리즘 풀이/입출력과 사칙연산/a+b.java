7b0e8479d9325ec9490ead40d4f310c6545d88f9
liveforone <88976237+liveforone@users.noreply.github.com>
2022-09-05 14:59:29 +0900

    Create a+b.java

---
+++ 알고리즘 풀이/입출력과 사칙연산/a+b.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+
+        int a = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+
+        System.out.println(a);
+    }
+}
