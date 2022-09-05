b0ab9278d3afa25a9719e2dfdc827b1c0c282243
liveforone <88976237+liveforone@users.noreply.github.com>
2022-09-05 15:01:10 +0900

    Update a+b.java

--- 알고리즘 풀이/입출력과 사칙연산/a+b.java
+++ 알고리즘 풀이/입출력과 사칙연산/a+b.java
@@ -1,3 +1,8 @@
+/*
+1. 한줄에 숫자 a와 b를 공백을 두고 입력받는다.
+2. 두 수를 더한 수를 출력한다.
+*/
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,7 +14,7 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
-        StringTokenizer st = new StringTokenizer(str, " ");
+        StringTokenizer st = new StringTokenizer(str, " ");  //성능면에서 좋은 토크나이저로 분류해준다.
 
         int a = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
 
